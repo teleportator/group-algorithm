c84f65d561cfa98c4eff5cadb7ae4b770b074561
teleportator <teleportator@ya.ru>
2017-11-23 18:33:02 +0300

    Initial commit

---
+++ GroupAlgorithm.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GroupAlgorithm
+{
+	public class GroupAlgorithm
+	{
+		public Tuple<int, int>[] Group(int[] array, int x)
+		{
+			var linkedList = new LinkedList<int>(array);
+			var pairs = new List<Tuple<int, int>>();
+			while (linkedList.First != null)
+			{
+				var first = linkedList.First;
+				var next = first.Next;
+				while (next != null)
+				{
+					if (first.Value + next.Value == x)
+					{
+						linkedList.Remove(next);
+						pairs.Add(new Tuple<int, int>(first.Value, next.Value));
+						break;
+					}
+
+					next = next.Next;
+				}
+
+				linkedList.Remove(first);
+			}
+
+			return pairs.ToArray();
+		}
+	}
+}
---
+++ GroupAlgorithmTests.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace GroupAlgorithm
+{
+	[TestClass]
+	public class GroupAlgorithmTests
+	{
+		[TestMethod]
+		public void GroupAlgorithm_SmokeTest()
+		{
+			var array = new [] { 1, 1, 2, 1, 1, 0, 1 };
+			var expected = new[] {2, 2, 2};
+			var initialValuesOccurance = Count(array);
+
+			var sut = new GroupAlgorithm();
+			var x = 2;
+			var actual = sut.Group(array, x);
+
+			actual.Select(p => p.Item1 + p.Item2).ShouldAllBeEquivalentTo(expected); // all pairs sum equals x
+			var valueOccurance = Count(actual.SelectMany(t => new[] {t.Item1, t.Item2}));
+			foreach (var item in valueOccurance)
+			{
+				item.Value.Should().BeLessOrEqualTo(initialValuesOccurance[item.Key]); // every element used in pair no more than once
+			}
+		}
+
+		private static ConcurrentDictionary<int, int> Count(IEnumerable<int> array)
+		{
+			return array.Aggregate(
+				new ConcurrentDictionary<int, int>(),
+				(dic, i) =>
+				{
+					dic.AddOrUpdate(i, 1, (k, v) => v + 1);
+					return dic;
+				},
+				dic => dic);
+		}
+	}
+}
